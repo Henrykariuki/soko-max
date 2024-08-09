1ea08acfd8d84735dfefcaa51ad0c1dfb116f1e4
Henry Kariuki <henrykariuki17@gmail.com>
2024-08-10 01:39:53 +0300

    Initial commit

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ShoppingCart } from 'lucide-vue-next';
+import { ShoppingCart, Search } from 'lucide-vue-next';
 import { ref } from 'vue';
 
 
@@ -14,18 +14,19 @@ const headerButtons =ref([
 
 <template>
   <div class="border border-black flex justify-between">
-    <div class="border border-black flex">
+    <div class="border border-black flex items-center">
       <img class="w-14 h-14" src="/images/sokologo.jpg">
-      <p class="font-bold text-2xl text-green-900">SokoMax</p>
+      <p class="font-bold text-2xl text-green-900">SokoMax</p> 
     </div>
     <div  class="border border-black flex flex-row gap-2 items-center">
       <div v-for="(info, index) in headerButtons" :key="`id-${index}`" class="hover:bg-gray-50 text-sm border border-black p-2 rounded-md">
         {{ info.name }}
       </div>
     </div>
-    <div class="border border-black flex items-center">
-      <input type="text" class="border border-gray-300 rounded-full">
+    <div class="border border-black relative flex flex-row gap-6 items-center">
+      <input type="text" placeholder="Search Product" class="border px-4 py-2 w-72  border-gray-300 rounded-full">
       <ShoppingCart/>
+      <button class="absolute right-16 text-gray-500"><Search size="16"/></button>
     </div>
   </div>
 </template>
