dc049c48c6b3e620a0eaecfa2207d2ddc2d9648c
Anthony Ngure <anthonyngure25@gmail.com>
2024-09-20 10:39:13 +0300

    Add app.vue

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
